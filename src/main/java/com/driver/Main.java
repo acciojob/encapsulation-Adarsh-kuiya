429779d91db024a323aae7da2f46f40482b06222
Adarsh kumar kuiya <adarshk9889@gmail.com>
2022-12-12 00:59:26 +0530

    encapsultion

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r= new RWOnly();
+        //r.name="hello";
+        //System.out.println(name);
+        //java: name has private access in com.driver.RWOnly
+        r.setName("RAM");
+        String ans= r.getName();
+        System.out.println(ans);
+
+    }
 }
